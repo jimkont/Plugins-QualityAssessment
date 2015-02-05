e81397ef26102289f95c59c20028a687841d9aba
anisaR <anisarula@anisas-MacBook-Pro.local>
2015-02-05 11:57:05 +0100

    enriched the ontology

--- QualityOntology/src/main/java/eu/unifiedviews/plugins/quality/qualitygraph/QualityOntology/QualityOntology.java
+++ QualityOntology/src/main/java/eu/unifiedviews/plugins/quality/qualitygraph/QualityOntology/QualityOntology.java
@@ -5,13 +5,18 @@ import org.openrdf.model.ValueFactory;
 
 public class QualityOntology {
 
-    public static final String RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-    public static final String DC = "http://purl.org/dc/elements/1.1#";
-    public static final String QB = "http://purl.org/linked-data/cube#";
-    public static final String DAQ = "http://purl.org/eis/vocab/daq#";
+    public static final String RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns/";
+    public static final String DC = "http://purl.org/dc/elements/1.1/";
+    public static final String DCTERMS = "http://purl.org/dc/terms/";
+    public static final String QB = "http://purl.org/linked-data/cube/";
+    public static final String DAQ = "http://purl.org/eis/vocab/daq/";
     public static final String EX = "http://comsode.eu/quality/";
 
     public static URI RDF_A_PREDICATE;
+    public static URI RDF_STATEMENT;
+    public static URI RDF_SUBJECT_PREDICATE;
+    public static URI RDF_PREDICATE_PREDICATE;
+    public static URI RDF_OBJECT_PREDICATE;
 
     public static URI DC_DATE;
 
@@ -26,14 +31,26 @@ public class QualityOntology {
     public static URI DAQ_METRIC;
     public static URI DAQ_COMPUTED_ON;
     public static URI DAQ_VALUE;
-
+    
+    public static URI DAQ_HAS_SEVERITY;
+    public static URI DCTERMS_PROBLEMDESCRIPTION;
+    
     public static URI EX_DPU_NAME;
     public static URI EX_TIMELINESS_DIMENSION;
-    public static URI[] EX_OBSERVATIONS;
+    public static URI EX_COMPLETENESS_DIMENSION;
+    public static URI EX_ACCURACY_DIMENSION;
+    public static URI EX_CONSISTENCY_DIMENSION;
+    //public static URI[] EX_OBSERVATIONS;
 
     public static void init(ValueFactory valueFactory, String nameDPU, int observationsNumber) {
 
         RDF_A_PREDICATE = valueFactory.createURI(RDF +"type");
+        RDF_SUBJECT_PREDICATE = valueFactory.createURI(RDF +"Statement");
+        RDF_SUBJECT_PREDICATE = valueFactory.createURI(RDF +"subject");
+        RDF_PREDICATE_PREDICATE = valueFactory.createURI(RDF +"predicate");
+        RDF_OBJECT_PREDICATE = valueFactory.createURI(RDF +"object");
+        
+        DCTERMS_PROBLEMDESCRIPTION = valueFactory.createURI(DCTERMS +"description");
 
         DC_DATE = valueFactory.createURI(DC +"date");
 
@@ -48,13 +65,19 @@ public class QualityOntology {
         DAQ_METRIC = valueFactory.createURI(DAQ +"Metric");
         DAQ_COMPUTED_ON = valueFactory.createURI(DAQ +"computedOn");
         DAQ_VALUE = valueFactory.createURI(DAQ +"value");
-
+        
+        DAQ_HAS_SEVERITY = valueFactory.createURI(DAQ +"severity");
+        
         EX_DPU_NAME = valueFactory.createURI(EX + nameDPU);
         EX_TIMELINESS_DIMENSION = valueFactory.createURI(EX +"timelinessDimension");
-        EX_OBSERVATIONS = new URI[observationsNumber + 1];
+        EX_COMPLETENESS_DIMENSION = valueFactory.createURI(EX +"completenessDimension");
+        EX_ACCURACY_DIMENSION = valueFactory.createURI(EX +"accuracyDimension");
+        EX_CONSISTENCY_DIMENSION = valueFactory.createURI(EX +"consistencyDimension");
+        
+        //EX_OBSERVATIONS = new URI[observationsNumber];
 
-        for (int i = 0; i <= observationsNumber; i++) {
-            EX_OBSERVATIONS[i] = valueFactory.createURI(EX +"obs"+ i);
-        }
+        /*for (int i = 1; i <= observationsNumber; i++) {
+            EX_OBSERVATIONS[i-1] = valueFactory.createURI(EX +"obs"+ i);
+        }*/
     }
 }
