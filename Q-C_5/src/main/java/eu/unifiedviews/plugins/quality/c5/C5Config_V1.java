a508b19a0b7b34635b3d98dd49dae65e3c4deab5
vcutrona <v.cutrona1@gmail.com>
2015-03-11 18:07:29 +0100

    Developed Q-C_5.

---
+++ Q-C_5/src/main/java/eu/unifiedviews/plugins/quality/c5/C5Config_V1.java
@@ -0,0 +1,44 @@
+package eu.unifiedviews.plugins.quality.c5;
+
+import java.util.ArrayList;
+
+/**
+ * Configuration class for C5.
+ *
+ * @author Vincenzo Cutrona
+ */
+public class C5Config_V1 {
+
+    private ArrayList<String> subject = new ArrayList<>();
+    private ArrayList<String> property = new ArrayList<>();
+    private ArrayList<String> properties = new ArrayList<>();
+
+    public C5Config_V1() {
+        properties.add("http://www.w3.org/2000/01/rdf-schema#label");
+        properties.add("http://www.w3.org/2000/01/rdf-schema#comment");
+        properties.add("http://purl.org/dc/elements/1.1/title");
+        properties.add("http://purl.org/rss/1.0/title");
+        properties.add("http://purl.org/dc/elements/1.1/description");
+    }
+
+    public ArrayList<String> getSubject() {
+        return subject;
+    }
+
+    public void setSubject(ArrayList<String> subject) {
+        this.subject = subject;
+    }
+
+    public ArrayList<String> getProperty() {
+        return property;
+    }
+
+    public void setProperty(ArrayList<String> property) {
+        this.property = property;
+    }
+
+    public ArrayList<String> getProperties() {
+        return properties;
+    }
+
+}
