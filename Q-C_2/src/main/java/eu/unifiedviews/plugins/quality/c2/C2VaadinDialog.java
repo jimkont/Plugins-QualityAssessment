4a583c558562f4f9eccdc48b33a95160a501ec43
Andrea Agazzone <a.agazzone@gmail.com>
2015-03-30 21:11:07 +0200

    Issue properly fixed: #10 , #11 , #12

--- Q-C_2/src/main/java/eu/unifiedviews/plugins/quality/c2/C2VaadinDialog.java
+++ Q-C_2/src/main/java/eu/unifiedviews/plugins/quality/c2/C2VaadinDialog.java
@@ -2,7 +2,7 @@ package eu.unifiedviews.plugins.quality.c2;
 
 import java.util.ArrayList;
 
-import com.vaadin.data.Validator;
+import com.vaadin.event.FieldEvents;
 import com.vaadin.server.UserError;
 import com.vaadin.ui.*;
 import com.vaadin.ui.Button.ClickEvent;
@@ -47,69 +47,9 @@ public class C2VaadinDialog extends AbstractDialog<C2Config_V1> {
 
             @Override
             public void buttonClick(ClickEvent event) {
-
-                boolean empty = false;
-
-                for (int i = 1; i < propertiesGridLayout.getRows(); i++) {
-                    String txtSubject = ((TextField) propertiesGridLayout.getComponent(0, i)).getValue();
-                    String txtProperty = ((TextField) propertiesGridLayout.getComponent(1, i)).getValue();
-                    if (txtSubject.isEmpty() || txtProperty.isEmpty()) {
-                        empty = true;
-                        if (txtSubject.isEmpty())
-                            ((TextField) propertiesGridLayout.getComponent(0, i)).setComponentError(new UserError(ctx.tr("C2.error.subject.not.filled")));
-                        else
-                            ((TextField) propertiesGridLayout.getComponent(0, i)).setComponentError(null);
-                        if (txtProperty.isEmpty())
-                            ((TextField) propertiesGridLayout.getComponent(1, i)).setComponentError(new UserError(ctx.tr("C2.error.property.not.filled")));
-                        else
-                            ((TextField) propertiesGridLayout.getComponent(1, i)).setComponentError(null);
-                    } else {
-                        ((TextField) propertiesGridLayout.getComponent(0, i)).setComponentError(null);
-                        ((TextField) propertiesGridLayout.getComponent(1, i)).setComponentError(null);
-                    }
-                }
-
-                if (!empty) {
-                    addColumnToPropertyMapping("http://", "http://");
-                }
+                addColumnToPropertyMapping("", "");
             }
         });
-
-        /*Button btnRemoveRow = new Button(ctx.tr("C2.button.remove"));
-        btnRemoveRow.addClickListener(new Button.ClickListener() {
-            private static final long serialVersionUID = -8609995802749728232L;
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                int lastRow = propertiesGridLayout.getRows() - 1;
-                String txtSubject = "";
-                String txtProperty = "";
-                if (lastRow > 0) {
-
-                    txtSubject = ((TextField) propertiesGridLayout.getComponent(0, lastRow)).getValue();
-                    txtProperty = ((TextField) propertiesGridLayout.getComponent(1, lastRow)).getValue();
-
-                    propertiesGridLayout.removeRow(lastRow);
-
-                    addColumnToPropertyMapping("http://", "http://");
-                }
-                if (lastRow > 1 && txtSubject.isEmpty() && txtProperty.isEmpty()) {
-                    propertiesGridLayout.removeRow(lastRow - 1);
-                }
-            }
-        });*/
-
-        /*Button btnRemoveRows = new Button(ctx.tr("C2.button.remove.all"));
-        btnRemoveRows.addClickListener(new Button.ClickListener() {
-            private static final long serialVersionUID = -8609995802749728232L;
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-
-                propertiesGridLayout.removeAllComponents();
-
-                addColumnToPropertyMappingsHeading();
-                addColumnToPropertyMapping("http://", "http://");
-            }
-        });*/
 
         baseFormLayout = new FormLayout();
         baseFormLayout.setSizeUndefined();
@@ -117,8 +57,6 @@ public class C2VaadinDialog extends AbstractDialog<C2Config_V1> {
         HorizontalLayout baseFormLayoutSecond = new HorizontalLayout();
         baseFormLayoutSecond.setSpacing(true);
         baseFormLayoutSecond.addComponent(btnAddRow);
-        //baseFormLayoutSecond.addComponent(btnRemoveRow);
-        //baseFormLayoutSecond.addComponent(btnRemoveRows);
 
         mainLayout.addComponent(baseFormLayoutSecond);
 
@@ -133,24 +71,42 @@ public class C2VaadinDialog extends AbstractDialog<C2Config_V1> {
 
     private void addColumnToPropertyMapping(String subject, String property) {
 
-        TextField txtSubject = new TextField();
+        final TextField txtSubject = new TextField();
         txtSubject.setValue(subject);
         txtSubject.setRequired(true);
         txtSubject.setWidth("100%");
-        txtSubject.addValidator(fieldValidator("subject"));
+        txtSubject.setInputPrompt("http://");
+        txtSubject.addBlurListener(new FieldEvents.BlurListener() {
+            @Override
+            public void blur(FieldEvents.BlurEvent blurEvent) {
+                int row = propertiesGridLayout.getComponentArea(txtSubject).getRow1();
+                fieldValidation(0, row);
+            }
+        });
 
-        TextField txtProperty = new TextField();
+        final TextField txtProperty = new TextField();
         txtProperty.setValue(property);
         txtProperty.setRequired(true);
         txtProperty.setWidth("100%");
-        txtProperty.addValidator(fieldValidator("property"));
+        txtProperty.setInputPrompt("http://");
+        txtProperty.addBlurListener(new FieldEvents.BlurListener() {
+            @Override
+            public void blur(FieldEvents.BlurEvent blurEvent) {
+                int row = propertiesGridLayout.getComponentArea(txtProperty).getRow1();
+                fieldValidation(1, row);
+            }
+        });
 
         final Button btnRemoveRow = new Button(ctx.tr("C2.button.remove"));
         btnRemoveRow.addClickListener(new Button.ClickListener() {
             private static final long serialVersionUID = -8609995802749728232L;
+
             @Override
             public void buttonClick(Button.ClickEvent event) {
                 propertiesGridLayout.removeRow(propertiesGridLayout.getComponentArea(btnRemoveRow).getRow1());
+                if (propertiesGridLayout.getRows() == 1) {
+                    addColumnToPropertyMapping("", "");
+                }
             }
         });
 
@@ -159,24 +115,46 @@ public class C2VaadinDialog extends AbstractDialog<C2Config_V1> {
         propertiesGridLayout.addComponent(btnRemoveRow);
     }
 
-    private Validator fieldValidator (final String field) {
-        return new Validator() {
-            private static final long serialVersionUID = 1L;
 
-            @Override
-            public void validate(Object value) throws InvalidValueException {
+    public String fieldsValidation () {
 
-                String myValue = value.toString().toLowerCase().trim();
+        String errors = "";
 
-                if (myValue.isEmpty()) {
-                    throw new InvalidValueException("\n"+ ctx.tr("C2.error."+ field +".not.filled"));
-                } else if (!myValue.startsWith("http://")) {
-                    throw new InvalidValueException("\n"+ ctx.tr("C2.error."+ field +".not.http"));
-                } else if (myValue.contains(" ")) {
-                    throw new InvalidValueException("\n"+ ctx.tr("C2.error."+ field +".whitespace"));
-                }
-            }
-        };
+        for (int i = 1; i < propertiesGridLayout.getRows(); i++) {
+            errors = errors + fieldValidation(0, i);
+            errors = errors + fieldValidation(1, i);
+        }
+
+        return errors;
+    }
+
+    public String fieldValidation (int column, int row) {
+        TextField txt = (TextField) propertiesGridLayout.getComponent(column, row);
+        String txtValue = txt.getValue().toLowerCase().trim();
+
+        String type;
+        if (column == 0) {
+            type = "subject";
+        } else {
+            type = "property";
+        }
+
+        String error = "";
+
+        if (txtValue.isEmpty()) {
+            error = error + "\n" + ctx.tr("C2.error."+ type +".not.filled") +" [Row: "+ row +"]";
+            txt.setComponentError(new UserError(ctx.tr("C2.error."+ type +".not.filled")));
+        } else if (!txtValue.startsWith("http://")) {
+            error = error + "\n" + ctx.tr("C2.error."+ type +".not.filled") +" [Row: "+ row +"]";
+            txt.setComponentError(new UserError(ctx.tr("C2.error."+ type +".not.http")));
+        } else if (txtValue.contains(" ")) {
+            error = error + "\n" + ctx.tr("C2.error."+ type +".not.filled") +" [Row: "+ row +"]";
+            txt.setComponentError(new UserError(ctx.tr("C2.error."+ type +".whitespace")));
+        } else {
+            txt.setComponentError(null);
+        }
+
+        return error;
     }
 
     @Override
@@ -190,7 +168,7 @@ public class C2VaadinDialog extends AbstractDialog<C2Config_V1> {
         this.addColumnToPropertyMappingsHeading();
 
         if (subject.size() == 0) {
-            this.addColumnToPropertyMapping("http://", "http://");
+            this.addColumnToPropertyMapping("", "");
         } else {
             for (int i = 0; i < subject.size(); i++) {
                 if (!subject.get(i).trim().equals("") && !property.get(i).trim().equals("")) {
@@ -208,49 +186,21 @@ public class C2VaadinDialog extends AbstractDialog<C2Config_V1> {
         ArrayList<String> subject = new ArrayList<>();
         ArrayList<String> property = new ArrayList<>();
 
-        String validation = "";
+        String error = fieldsValidation();
 
-        int row = 1;
-        while (row < propertiesGridLayout.getRows()) {
+        if (!error.isEmpty()) {
+            throw new DPUConfigException(error);
+        } else {
+            for (int row = 1; row < propertiesGridLayout.getRows(); row++) {
+                TextField txtSubject = ((TextField) propertiesGridLayout.getComponent(0, row));
+                TextField txtProperty = ((TextField) propertiesGridLayout.getComponent(1, row));
 
-            TextField txtSubject = ((TextField) propertiesGridLayout.getComponent(0, row));
-            TextField txtProperty = ((TextField) propertiesGridLayout.getComponent(1, row));
+                String txtSubjectValue = txtSubject.getValue().trim();
+                String txtPropertyValue = txtProperty.getValue().trim();
 
-            String txtSubjectValue = txtSubject.getValue().trim();
-            String txtPropertyValue = txtProperty.getValue().trim();
-
-            if (txtSubjectValue.isEmpty()) {
-                validation = validation +"\n"+ ctx.tr("C2.error.subject.not.filled");
+                subject.add(row - 1, txtSubjectValue);
+                property.add(row - 1, txtPropertyValue);
             }
-
-            if (txtPropertyValue.isEmpty()) {
-                validation = validation +"\n"+ ctx.tr("C2.error.property.not.filled");
-            }
-
-            if (!txtSubject.isValid()) {
-                try {
-                    txtSubject.validate();
-                } catch (Validator.InvalidValueException e) {
-                    validation = validation + e.getMessage();
-                }
-            }
-
-            if (!txtProperty.isValid()) {
-                try {
-                    txtProperty.validate();
-                } catch (Validator.InvalidValueException e) {
-                    validation = validation + e.getMessage();
-                }
-            }
-
-            if (validation.isEmpty()) {
-                subject.add(row-1, txtSubjectValue);
-                property.add(row-1, txtPropertyValue);
-            } else {
-                throw new DPUConfigException(validation);
-            }
-
-            row++;
         }
 
         config.setSubject(subject);
