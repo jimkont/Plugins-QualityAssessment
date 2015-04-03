872cb20415b5b6e906c1078bd3b7431c91fb3158
Andrea Agazzone <a.agazzone@gmail.com>
2015-04-03 10:43:29 +0200

    Fixed Issue: #16

--- Q-ACC_4/src/main/java/eu/unifiedviews/plugins/quality/acc4/ACC4.java
+++ Q-ACC_4/src/main/java/eu/unifiedviews/plugins/quality/acc4/ACC4.java
@@ -7,7 +7,7 @@ import java.util.Date;
 import eu.unifiedviews.helpers.dpu.context.ContextUtils;
 import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
-import org.openrdf.model.vocabulary.DC;
+import org.openrdf.model.vocabulary.DCTERMS;
 import org.openrdf.model.vocabulary.RDF;
 import org.openrdf.repository.RepositoryConnection;
 import eu.unifiedviews.dataunit.DataUnit;
@@ -196,7 +196,7 @@ public class ACC4 extends AbstractDpu<ACC4Config_V1> {
         observationEntity
                 .property(RDF.TYPE, QualityOntology.QB_OBSERVATION)
                 .property(QualityOntology.DAQ_COMPUTED_ON, valueFactory.createURI(obs_bnode))
-                .property(DC.DATE, valueFactory.createLiteral(reportDate))
+                .property(DCTERMS.DATE, valueFactory.createLiteral(reportDate))
                 .property(QualityOntology.DAQ_VALUE, valueFactory.createLiteral(value));
 
         return observationEntity;
